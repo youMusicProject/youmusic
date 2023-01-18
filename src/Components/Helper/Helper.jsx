c0ea8e078883989d6f33bf20e2303a97ad5d71e3
Alvaro Alonso <airuritac@gmail.com>
2023-01-18 12:04:07 +0100

    fix log ddbb

--- src/Components/Helper/Helper.jsx
+++ src/Components/Helper/Helper.jsx
@@ -13,7 +13,7 @@ export const Helper = () => {
     const dispatch = useDispatch();
     const { getAccessTokenSilently, user } = useAuth0();
     const serverUrl = process.env.REACT_APP_SERVER_URL;
-    
+
     useEffect(() => {
         // fetchGet(dispatch, "user", setUserList);
         fetchGet(dispatch, "track", setTracksList);
@@ -22,7 +22,7 @@ export const Helper = () => {
         fetchGet(dispatch, "artist", setArtistsList);
         // fetchGetGenresList(dispatch);
     }, [dispatch])
-    
+
     useEffect(() => {
         if (user) {
             checkUser();
@@ -32,44 +32,47 @@ export const Helper = () => {
     const checkUser = async () => {
         // PETICION AL BACKEND
         const token = await getAccessTokenSilently();
+
         const response = await fetch(`${serverUrl}/api/user/checkuser/${user.email}`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
         })
         const responseData = await response.json()
-        console.log(responseData.info[0]);
-        // CONDICIONAL - EL BACK NOS DEVUELVE TRUE O FALSE
-        if (!!responseData.user) {
-            console.log('El usuario existe en la bbdd');
-            // console.log(responseData.user[0]);
-            await dispatch(setUserLogged(responseData.info[0]));
-            // setuserLoged(responseData.user[0])
-        } //else {
-        //     // SI NO EXISTE, CREAR USER CON POST
-        //     console.log('El usuario no existe en la bbdd');
-        //     // TENEMOS QUE CREAR ESTE USUARIO
-        //     const $user = {
-        //         email: user.email,
-        //         userData: {
-        //             username: user.nickname,
-        //             first_name: user.given_name,
-        //             last_name: user.family_name,
-        //             profilePicture: user.picture 
-        //         }
-        //     }
+        // console.log(responseData.info[0]);
 
-        // const request = await fetch(`${serverUrl}/api/user/createuser`, {
-        //     method: "POST",
-        //     body: JSON.stringify($user),
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //         Authorization: `Bearer ${token}`
-        //     }
-        // });
-        // const data = await request.json()
-        // console.log(data.post);
-        // setuserLoged(data.post)
-        // setMessage(data.mensaje);
+        // CONDICIONAL - EL BACK NOS DEVUELVE TRUE O FALSE
+        if (!!responseData.info[0]) {
+            console.log('El usuario existe en la bbdd');
+            await dispatch(setUserLogged(responseData.info[0]));
+        } else {
+            // SI NO EXISTE, CREAR USER CON POST
+            console.log('El usuario no existe en la bbdd');
+            // TENEMOS QUE CREAR ESTE USUARIO
+            const $user = {
+                email: user.email,
+                userData: {
+                    username: user.nickname,
+                    first_name: user.given_name,
+                    last_name: user.family_name,
+                    profilePicture: user.picture,
+                    complete_name: user.name
+                }
+            }
+
+            const request = await fetch(`${serverUrl}/api/user/createuser`, {
+                method: "POST",
+                body: JSON.stringify($user),
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            const data = await request.json()
+            console.log(data);
+            await dispatch(setUserLogged(data.info));
+            // setuserLoged(data.post);
+            // setMessage(data.mensaje);
+        }
     }
 }
