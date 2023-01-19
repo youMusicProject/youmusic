d2bde4d769380f51446e6cec70bcd8f3b226add3
Juan Carlos M Giner <jcmginer@gmail.com>
2023-01-19 09:36:20 +0100

    genre

--- src/Components/Helper/Helper.jsx
+++ src/Components/Helper/Helper.jsx
@@ -6,6 +6,9 @@ import { useDispatch } from 'react-redux';
 import { setTracksList } from "../../redux/features/tracks/tracksSlice";
 import { setArtistsList } from "../../redux/features/artists/artistsSlice";
 import { setPlaylistsList } from "../../redux/features/playlist/playlistSlice";
+import { setAlbumsList } from "../../redux/features/albums/albumsSlice";
+import { setGenresList } from "../../redux/features/genres/genresSlice";
+
 
 
 export const Helper = () => {
@@ -13,9 +16,10 @@ export const Helper = () => {
 
     useEffect(() => {
         fetchGet(dispatch, "track", setTracksList);
-        // fetchGetAlbums(dispatch);
+        fetchGet(dispatch, "album", setAlbumsList);
         fetchGet(dispatch, "playlist", setPlaylistsList);
         fetchGet(dispatch, "artist", setArtistsList);
+        fetchGet(dispatch, "genre", setGenresList);
         // fetchGetGenresList(dispatch);
     }, [dispatch])
 
